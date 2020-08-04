20ef3c21889f6f33c36fbe298def05af373c4acf
vipul906 <vipuljain906@gmail.com>
2020-08-04 14:07:08 +0530

    Updates

---
+++ precache-manifest.9e325608eb52190a6f8f07fb5dec6d9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03f7dd2cda04e3d7ac6844e7319cfc0b",
+    "url": "/foodlife/index.html"
+  },
+  {
+    "revision": "725c0faf53a15902c958",
+    "url": "/foodlife/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4f5f64c076176eea4af9",
+    "url": "/foodlife/static/js/2.2c631b0c.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/foodlife/static/js/2.2c631b0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "725c0faf53a15902c958",
+    "url": "/foodlife/static/js/main.eec8bae4.chunk.js"
+  },
+  {
+    "revision": "46640e3a952716d19f2c",
+    "url": "/foodlife/static/js/runtime-main.9c29e903.js"
+  }
+]);
